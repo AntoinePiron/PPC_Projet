947d27ebbf5a7f89003136e65202ed386ae1ac01
AntoinePiron <pro.piron.antoine@gmail.com>
2022-01-12 16:12:23 +0100

    ajout modification serveur

--- Code/Antoine/server.py
+++ Code/Antoine/server.py
@@ -6,7 +6,7 @@ import os
 if __name__ == "__main__":
     pid = os.getpid()
     print("main pid : ", pid)
-    currentOffers = shared_memory.ShareableList(["0;0","0;0","0;0"], name="currentOffers")
+    currentOffers = shared_memory.ShareableList(["0;0","0;0","0;0"], name="currentOffers", create=True)
     print(list(currentOffers))
     while True: 
         time.sleep(5)
