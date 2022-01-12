3edce0e1803198eb94ee95fd139e5f1635c642a2
AntoinePiron <pro.piron.antoine@gmail.com>
2022-01-12 15:05:08 +0100

    better client

--- Code/Antoine/client.py
+++ Code/Antoine/client.py
@@ -1,24 +1,47 @@
 from multiprocessing.process import current_process
 import os
 from multiprocessing import shared_memory
+import time
 from utils import *
+
+playerID = 0
+pid = 0
+
+def updateOffers(numberOfCards, shm):
+    shm[playerID-1] = pid.__str__() + ";" + numberOfCards.__str__()
 
 if __name__ == "__main__":
     currentOffers = shared_memory.ShareableList(name="currentOffers")
     pid = os.getpid()
-    id = 0
     while True:
         try:
-            id = int(input("Number of player: "))
-            if id < 1 or id > len(list(currentOffers)) :
+            playerID = int(input("Number of player: "))
+            if playerID < 1 or playerID > len(list(currentOffers)) :
                 print("Please enter a valid number : [1,%s]"%(len(list(currentOffers))))
+                continue
             else:
-                if currentOffers[id-1] == "0;0":
-                    currentOffers[id-1] = pid.__str__() + ";0"
+                if currentOffers[playerID-1] == "0;0":
+                    currentOffers[playerID-1] = pid.__str__() + ";0"
                     break
                 else:
-                    print("Number already attributed")
+                    print("Player number already attributed")
+                    continue
         except:
             print("Please enter a valid number.")
     while True:
-        pass
+        answer = input("Update offer ? [y/n]")
+        answer = answer.lower()
+        if answer == "y":
+            while True:
+                try:
+                    cards = int(input("Number of cards : "))
+                    if cards < 1 or cards > 5 :
+                        print("Please enter a valid number : [1,5]")
+                        continue
+                    else:
+                        updateOffers(cards, currentOffers)
+                        break
+                except:
+                    print("Please enter a valid number.")
+        else:
+            time.sleep(5)
