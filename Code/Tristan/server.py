44267f837bbf22ec9776986eb65974a79b5e1d2c
TristanDevin <tristan.devin@insa-lyon.fr>
2022-01-12 16:36:07 +0100

    code lancement jeu

--- Code/Tristan/server.py
+++ Code/Tristan/server.py
@@ -3,16 +3,19 @@ import time
 
 
 key = 128
+debutkey = 100
 
 mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+md = sysv_ipc.MessageQueue(debutkey, sysv_ipc.IPC_CREAT)
+
 pid = []
 
 while True:
-    print(pid)
-    message, t = mq.receive()
-    value = message.decode()
-    print(value)
-    if not (pid.count(value) > 1):
-        pid.append(value)
+    while len(pid) < 3:
+        message, t = md.receive()
+        value = message.decode()
+        if not (pid.count(value) > 1):
+            pid.append(value)
+            md.send(len(pid))
     
     
