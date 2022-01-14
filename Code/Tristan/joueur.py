b027e10df2044920860542f44ac5b43898e46039
TristanDevin <tristan.devin@insa-lyon.fr>
2022-01-14 19:09:45 +0100

    adding comments

--- Code/Tristan/joueur.py
+++ Code/Tristan/joueur.py
@@ -5,26 +5,29 @@ from utils import *
 import os
 import sysv_ipc
 
-key = 128
-debutkey = 100
+key = 128 # Declaration of the key for the message queues
+debutkey = 100 #Could be passed to console args ?
+
 Pid = os.getpid
 
 
-
+#Fonction self-explainatory, waits for enough ppl to join the game
 def wait(value, md):
     while(value < 3):
         print("You are now in a queue, waiting for at least 3 players to join")
         print("There are currently " + str(value) + " players connected")
-        waiting, t = md.receive(type = 1)
-        value = int(waiting.decode())
+        waiting, t = md.receive(type = 2)  # Waits for connection ack type messages
+        value = int(waiting.decode()) 
 
-        if t == 1:
+        if t == 1: # If a connection type message is detected, update the number of player in the game
             print("New connection detected")
             print("There are now " + str(value) + " players connected")
+        
+    #If there are more than 3 players, breaks the loop   
     print("There are enough players to start the game")
     print("Sending a acceptation message")
     
-    md.send(str(1).encode(),type = 2)
+    md.send(str(1).encode(),type = 3)   #Sends a type 3 message, to ack the start of the game
     
     print("Starting game")
     game()
@@ -33,16 +36,16 @@ def wait(value, md):
 def game():
     while True:
         pass
+    
+    
     #Handle the joining the server, and sending the process' pid
-    
-    
-    
 def joinserver(pid):
     print("Hello and Welcome to the Cambiecolo game !")
+    
     try: #Will try to join an existing debut queue, starting connnection if succesful 
         md = sysv_ipc.MessageQueue(debutkey)
         print("Your pid will now be sent to the server for you to be in the game")
-        try:
+        try: #Ask player to send its pid, easy stuff
             type = int(input("1 for sending pid, pressing another key will result in termination : "))
             if type != 1 and type != 2:
                 print("Wrong values")
@@ -51,12 +54,13 @@ def joinserver(pid):
             print("An error occured")
             exit(1)
             
-        if type == 1:
-            md.send(str(pid).encode())
-            nbjoueur, t = md.receive(type = 1)
-            value = int(nbjoueur.decode())
+        if type == 1: #IF user,types one, sends pid to server, than wait for ack of connection
+            md.send(str(pid).encode(), type = 1)
+            nbjoueur, t = md.receive(type = 2)
+            value = int(nbjoueur.decode()) #Receive ack, with value being the number of player already connected
             wait(value, md)
-    except sysv_ipc.ExistentialError:     
+            
+    except sysv_ipc.ExistentialError:     #If the debut MessageQueue is not found, exits the process
         print("Could no connect to server")
         print("Either server not launched, or game already staarted")
         print("Exiting")
--- Code/Tristan/server.py
+++ Code/Tristan/server.py
@@ -5,35 +5,40 @@ import time
 
 
 ListePid = []
-debutkey = 100
+key = 128   # Declaration of the key for the message queues
+debutkey = 100 #Could be passed to console args ?
 
-#Cette création ne sert à rien, elle est simplement là pour vider la message queue du début pour éviter les problèmes, c'est temporaire
+
 def debutjeu():
-    print(ListePid)
     print("Server has been launched, waiting for connections")
-    md = sysv_ipc.MessageQueue(debutkey, sysv_ipc.IPC_CREAT)
-
+    
+    md = sysv_ipc.MessageQueue(debutkey, sysv_ipc.IPC_CREAT) #Creating debut message queue
+    #While there are less than 3 connection, wait for connection type messages
     while (len(ListePid) < 3):
-        connection, t = md.receive(type = 0)
-        print("New connection")
-        pid = connection.decode()
+        connection, t = md.receive(type = 1)
+        print("New connection") 
+        pid = connection.decode() #When a conncetion is detected, adds its pid to the list
         ListePid.append(pid)
-        for i in range(len(ListePid)):
+        for i in range(len(ListePid)): #Sends a connection ack to every player
             print(".")
-            md.send(str(len(ListePid)).encode(), type = 1)
+            md.send(str(len(ListePid)).encode(), type = 2)
+    #When there are more than 3 players, stops waiting for connections
+    
     print("We have 3 players !")
     print("We will now wait for every player to accept the start of the game")
     greenflag = 0
-    while (greenflag < len(ListePid)):
-        value, t = md.receive(type = 2)
+    while (greenflag < len(ListePid)): #Waits for every players to ack the start of the gaame
+        value, t = md.receive(type = 3)
         flag = int(value.decode())
-        greenflag = flag + greenflag
+        greenflag = flag + greenflag #Value of flag message should always be one (for now)
         print(greenflag)
+        
     print("Starting message queue deleted, game starting")
     md.remove()
     game()
         
         
+        #Fonction game, placeholder for now
 def game():
     while True:
         pass    
